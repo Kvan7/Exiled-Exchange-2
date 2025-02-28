6b3ea61b7f43df6b265a8d16aa952fee55b38eec
kvan7 <kvan.valve@gmail.com>
2025-02-27 18:20:10 -0600

    version bump

--- renderer/src/web/price-check/PriceCheckWindow.vue
+++ renderer/src/web/price-check/PriceCheckWindow.vue
@@ -193,6 +193,7 @@ export default defineComponent({
         autoFillEmptyRuneSockets: false,
         tierNumbering: "poe2",
         alwaysShowTier: false,
+        rememberRatio: false,
       };
     },
   } satisfies WidgetSpec,
