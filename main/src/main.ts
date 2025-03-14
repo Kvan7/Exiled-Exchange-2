309f68a8916118ecfea6d7f74f08cc14c18b1729
Tadeusz Makuch <tadeusz.makuch@gmail.com>
2025-03-14 21:24:14 +0100

    chore: safe setup in case electron app is ready before the permissions

--- main/src/main.ts
+++ main/src/main.ts
@@ -27,75 +27,79 @@ if (process.platform !== "darwin") {
 app.enableSandbox();
 let tray: AppTray;
 
+// eslint-disable-next-line @typescript-eslint/promise-function-async
 ensureAccessibilityPermission().then(() => {
-  app.on("ready", async () => {
-    tray = new AppTray(eventPipe);
-    const logger = new Logger(eventPipe);
-    const gameLogWatcher = new GameLogWatcher(eventPipe, logger);
-    const gameConfig = new GameConfig(eventPipe, logger);
-    const poeWindow = new GameWindow();
-    const appUpdater = new AppUpdater(eventPipe);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const _httpProxy = new HttpProxy(server, logger);
+  if (app.isReady()) {
+    return setupApp();
+  }
 
-    if (process.env.VITE_DEV_SERVER_URL) {
-      try {
-        await installExtension(VUEJS_DEVTOOLS);
-        logger.write("info Vue Devtools installed");
-      } catch (error) {
-        logger.write(`error installing Vue Devtools: ${error}`);
-        console.log(`error installing Vue Devtools: ${error}`);
-      }
-    }
-
-    setTimeout(
-      async () => {
-        const overlay = new OverlayWindow(eventPipe, logger, poeWindow);
-        // eslint-disable-next-line no-new
-        new OverlayVisibility(eventPipe, overlay, gameConfig);
-        const shortcuts = await Shortcuts.create(
-          logger,
-          overlay,
-          poeWindow,
-          gameConfig,
-          eventPipe,
-        );
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const filterGenerator = new FilterGenerator(
-          logger,
-          gameConfig,
-          eventPipe,
-        );
-        eventPipe.onEventAnyClient(
-          "CLIENT->MAIN::update-host-config",
-          (cfg) => {
-            overlay.updateOpts(cfg.overlayKey, cfg.windowTitle);
-            shortcuts.updateActions(
-              cfg.shortcuts,
-              cfg.stashScroll,
-              cfg.logKeys,
-              cfg.restoreClipboard,
-              cfg.language,
-            );
-            gameLogWatcher.restart(cfg.clientLog ?? "");
-            gameConfig.readConfig(cfg.gameConfig ?? "");
-            appUpdater.checkAtStartup();
-            tray.overlayKey = cfg.overlayKey;
-          },
-        );
-        uIOhook.start();
-        console.log("uIOhook started");
-        const port = await startServer(appUpdater, logger);
-        // TODO: move up (currently crashes)
-        logger.write(`info ${os.type()} ${os.release} / v${app.getVersion()}`);
-        overlay.loadAppPage(port);
-        tray.serverPort = port;
-      },
-      // fixes(linux): window is black instead of transparent
-      process.platform === "linux" ? 1000 : 0,
-    );
-  });
+  app.on("ready", setupApp);
 });
+
+async function setupApp() {
+  tray = new AppTray(eventPipe);
+  const logger = new Logger(eventPipe);
+  const gameLogWatcher = new GameLogWatcher(eventPipe, logger);
+  const gameConfig = new GameConfig(eventPipe, logger);
+  const poeWindow = new GameWindow();
+  const appUpdater = new AppUpdater(eventPipe);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const _httpProxy = new HttpProxy(server, logger);
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    try {
+      await installExtension(VUEJS_DEVTOOLS);
+      logger.write("info Vue Devtools installed");
+    } catch (error) {
+      logger.write(`error installing Vue Devtools: ${error}`);
+      console.log(`error installing Vue Devtools: ${error}`);
+    }
+  }
+
+  setTimeout(
+    async () => {
+      const overlay = new OverlayWindow(eventPipe, logger, poeWindow);
+      // eslint-disable-next-line no-new
+      new OverlayVisibility(eventPipe, overlay, gameConfig);
+      const shortcuts = await Shortcuts.create(
+        logger,
+        overlay,
+        poeWindow,
+        gameConfig,
+        eventPipe,
+      );
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const filterGenerator = new FilterGenerator(
+        logger,
+        gameConfig,
+        eventPipe,
+      );
+      eventPipe.onEventAnyClient("CLIENT->MAIN::update-host-config", (cfg) => {
+        overlay.updateOpts(cfg.overlayKey, cfg.windowTitle);
+        shortcuts.updateActions(
+          cfg.shortcuts,
+          cfg.stashScroll,
+          cfg.logKeys,
+          cfg.restoreClipboard,
+          cfg.language,
+        );
+        gameLogWatcher.restart(cfg.clientLog ?? "");
+        gameConfig.readConfig(cfg.gameConfig ?? "");
+        appUpdater.checkAtStartup();
+        tray.overlayKey = cfg.overlayKey;
+      });
+      uIOhook.start();
+      console.log("uIOhook started");
+      const port = await startServer(appUpdater, logger);
+      // TODO: move up (currently crashes)
+      logger.write(`info ${os.type()} ${os.release} / v${app.getVersion()}`);
+      overlay.loadAppPage(port);
+      tray.serverPort = port;
+    },
+    // fixes(linux): window is black instead of transparent
+    process.platform === "linux" ? 1000 : 0,
+  );
+}
 
 async function ensureAccessibilityPermission() {
   // windows & linux does not require special permissions
