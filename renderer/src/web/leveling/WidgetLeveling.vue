b0055467c00b8d136ddd1039d6af463793b64c72
kvan7 <kvan.valve@gmail.com>
2025-12-11 08:39:49 -0600

    generic rigging setup

--- renderer/src/web/leveling/WidgetLeveling.vue
+++ renderer/src/web/leveling/WidgetLeveling.vue
@@ -1,15 +1,32 @@
 <template>
   <Widget :config="config" :removable="false" move-handles="top-bottom">
-    <div class="widget-default-style min-w-40 min-h-10">SOME TEXT</div>
+    <div
+      class="widget-default-style min-w-48 min-h-10"
+      :class="$style['container']"
+    >
+      <input
+        :class="$style['rollInput']"
+        :placeholder="t('min')"
+        min="1"
+        max="100"
+        step="1"
+        type="number"
+        v-model.number="playerLevel"
+        @mousewheel.stop
+      />
+      <div v-if="expDisplay">{{ expDisplay + playerLevel }}%</div>
+      <div>Over: {{ playerLevel }}</div>
+    </div>
   </Widget>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType, ref } from "vue";
 
 import Widget from "../overlay/Widget.vue";
 import { WidgetSpec } from "../overlay/interfaces";
 import { LevelingWidget } from "./widget";
+import { useI18n } from "vue-i18n";
 
 export default defineComponent({
   widget: {
@@ -28,6 +45,7 @@ export default defineComponent({
           x: 68,
           y: 98,
         },
+        showExp: true,
       };
     },
   } satisfies WidgetSpec,
@@ -38,5 +56,77 @@ export default defineComponent({
       required: true,
     },
   },
+  setup(props) {
+    const _playerLevel = ref<number | "">(1);
+    const _monsterLevel = ref<number>(1);
+
+    const playerLevel = computed({
+      get() {
+        return _playerLevel.value;
+      },
+      set(value: "" | number) {
+        console.log(value);
+        _playerLevel.value = value;
+      },
+    });
+
+    const expDisplay = computed(() => {
+      if (props.config.showExp) {
+        if (
+          _monsterLevel.value > 70 ||
+          (playerLevel.value !== "" && playerLevel.value >= 95)
+        ) {
+          return "Exp*: ";
+        }
+        return "Exp: ";
+      } else {
+        return false;
+      }
+    });
+
+    const { t } = useI18n();
+
+    return {
+      t,
+      playerLevel,
+      expDisplay,
+    };
+  },
 });
 </script>
+
+<style lang="postcss" module>
+.rollInput {
+  @apply bg-gray-900;
+  @apply text-gray-300;
+  @apply text-center;
+  @apply w-12;
+  @apply px-1;
+  @apply border border-transparent;
+
+  &:first-child {
+    @apply rounded-l;
+  }
+  &:last-child {
+    @apply rounded-r;
+  }
+
+  &::placeholder {
+    @apply text-gray-700;
+    font-size: 0.8125rem;
+  }
+
+  /* &:not(:placeholder-shown) { @apply border-gray-600; } */
+
+  &:focus {
+    @apply border-gray-500;
+    cursor: none;
+  }
+}
+
+.container {
+  @apply flex flex-row;
+  @apply justify-between items-center;
+  @apply px-2;
+}
+</style>
--- renderer/src/web/leveling/widget.ts
+++ renderer/src/web/leveling/widget.ts
@@ -2,4 +2,5 @@ import { Anchor, Widget } from "../overlay/widgets";
 
 export interface LevelingWidget extends Widget {
   anchor: Anchor;
+  showExp: boolean;
 }
