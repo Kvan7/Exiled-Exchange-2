91cf6e69aefd83905a5e27a22ade9f1b8e963638
kvan7 <kvan.valve@gmail.com>
2025-12-11 09:58:42 -0600

    penalty

--- renderer/src/web/leveling/WidgetLeveling.vue
+++ renderer/src/web/leveling/WidgetLeveling.vue
@@ -1,11 +1,13 @@
 <template>
-  <Widget :config="config" :removable="false" move-handles="top-bottom">
-    <div
-      class="widget-default-style min-w-48 min-h-10"
-      :class="$style['container']"
-    >
+  <Widget
+    :config="config"
+    :removable="false"
+    :inline-edit="false"
+    move-handles="top-bottom"
+  >
+    <div class="min-w-48 min-h-10" :class="$style['levelingContainer']">
       <input
-        :class="$style['rollInput']"
+        :class="$style['levelingInput']"
         :placeholder="t('min')"
         min="1"
         max="100"
@@ -14,8 +16,8 @@
         v-model.number="playerLevel"
         @mousewheel.stop
       />
-      <div v-if="expDisplay">{{ expDisplay + playerLevel }}%</div>
-      <div>Over: {{ playerLevel }}</div>
+      <div v-if="expDisplay">{{ expDisplay + expPenalty }}%</div>
+      <div>Over: {{ overIdeal }}</div>
     </div>
   </Widget>
 </template>
@@ -27,6 +29,32 @@ import Widget from "../overlay/Widget.vue";
 import { WidgetSpec } from "../overlay/interfaces";
 import { LevelingWidget } from "./widget";
 import { useI18n } from "vue-i18n";
+
+function calcBaseSafeZone(playerLevel: number): number {
+  return Math.floor(playerLevel / 16) + 3;
+}
+
+function getOverIdeal(playerLevel: number, monsterLevel: number): number {
+  const safeZone = calcBaseSafeZone(playerLevel);
+  return safeZone - (monsterLevel - playerLevel);
+}
+
+function getExpPenalty(playerLevel: number, monsterLevel: number): number {
+  const safeZone =
+    monsterLevel > playerLevel ? calcBaseSafeZone(playerLevel) : 0;
+
+  const effectiveDiff = Math.max(
+    Math.abs(monsterLevel - playerLevel) - safeZone,
+    0,
+  );
+
+  const expMulti = Math.max(
+    0.01,
+    ((playerLevel + 5) / (playerLevel + 5 + effectiveDiff ** 2.5)) ** 1.3,
+  );
+
+  return 100 * expMulti;
+}
 
 export default defineComponent({
   widget: {
@@ -57,14 +85,14 @@ export default defineComponent({
     },
   },
   setup(props) {
-    const _playerLevel = ref<number | "">(1);
-    const _monsterLevel = ref<number>(1);
+    const _playerLevel = ref<number>(1);
+    const _monsterLevel = ref<number>(42);
 
     const playerLevel = computed({
       get() {
         return _playerLevel.value;
       },
-      set(value: "" | number) {
+      set(value: number) {
         console.log(value);
         _playerLevel.value = value;
       },
@@ -72,10 +100,7 @@ export default defineComponent({
 
     const expDisplay = computed(() => {
       if (props.config.showExp) {
-        if (
-          _monsterLevel.value > 70 ||
-          (playerLevel.value !== "" && playerLevel.value >= 95)
-        ) {
+        if (_monsterLevel.value > 70 || playerLevel.value >= 95) {
           return "Exp*: ";
         }
         return "Exp: ";
@@ -90,26 +115,26 @@ export default defineComponent({
       t,
       playerLevel,
       expDisplay,
+      expPenalty: computed(() => {
+        return getExpPenalty(playerLevel.value, _monsterLevel.value).toFixed(1);
+      }),
+      overIdeal: computed(() => {
+        return getOverIdeal(playerLevel.value, _monsterLevel.value);
+      }),
     };
   },
 });
 </script>
 
 <style lang="postcss" module>
-.rollInput {
+.levelingInput {
   @apply bg-gray-900;
   @apply text-gray-300;
   @apply text-center;
-  @apply w-12;
+  @apply w-8;
   @apply px-1;
   @apply border border-transparent;
-
-  &:first-child {
-    @apply rounded-l;
-  }
-  &:last-child {
-    @apply rounded-r;
-  }
+  @apply rounded;
 
   &::placeholder {
     @apply text-gray-700;
@@ -124,9 +149,16 @@ export default defineComponent({
   }
 }
 
-.container {
+.levelingContainer {
   @apply flex flex-row;
   @apply justify-between items-center;
   @apply px-2;
+
+  @apply rounded;
+  @apply bg-gray-800;
+  @apply border-4 border-gray-900;
+  box-shadow:
+    0 1px 3px 0 rgba(0, 0, 0, 0.75),
+    0 1px 2px 0 rgba(0, 0, 0, 0.75);
 }
 </style>
