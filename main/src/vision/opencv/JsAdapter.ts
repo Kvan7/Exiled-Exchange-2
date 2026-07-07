4f8c29e33d8324d8012c5d3a6d4f6d82332f39d2
Kvan7 <71402892+Kvan7@users.noreply.github.com>
2026-07-07 13:09:39 -0500

    Update JsAdapter.ts

--- main/src/vision/opencv/JsAdapter.ts
+++ main/src/vision/opencv/JsAdapter.ts
@@ -1,17 +1,35 @@
 import type openCv from "@techstark/opencv-js";
-import type { ICvAdapter, ICvMat } from "./ICvAdapter";
+import type { ICvAdapter } from "./ICvAdapter";
 import { loadImage } from "canvas";
+import type { Mat } from "@techstark/opencv-js";
+import type { ImageData, BoundingBox } from "../utils";
 
 export class JsCvAdapter implements ICvAdapter {
   constructor(private _cv: typeof openCv) {}
 
-  async loadImage(input: string | Buffer): Promise<ICvMat> {
+  //#region Interface Methods
+  async calibrate(screenshot: ImageData): Promise<BoundingBox> {
+    const img = this.loadImage(Buffer.from(screenshot.data));
+    console.log(img);
+    throw new Error("Method not implemented.");
+  }
+  async findRecipeId(
+    screenshot: ImageData,
+    bbox: BoundingBox,
+  ): Promise<{
+    highlightedTome: string;
+    highlightedSlot: number;
+    tomeCount: number;
+  }> {
+    throw new Error("Method not implemented.");
+  }
+
+  //#endregion Interface Methods
+  //#region Private Methods
+  private async loadImage(input: string | Buffer): Promise<Mat> {
     const image = await loadImage(input);
     const mat = this._cv.imread(image as unknown as HTMLImageElement);
-    return new JsMatAdapter(mat);
+    return mat;
   }
-}
-
-export class JsMatAdapter implements ICvMat {
-  constructor(private _mat: any) {}
+  //#endregion Private Methods
 }
